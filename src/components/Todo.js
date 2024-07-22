653e5bb22165f57a34ff5ee94a44502f4da8fd95
arbiab <arbabdullahu2005@gmail.com>
2024-07-22 16:38:10 +0200

    Add files via upload

---
+++ src/components/Todo.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState } from 'react'
+import Modal from './Modal'
+import Backdrop from './Backdrop'
+const Todo = ({ title }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const deleteHandler = () => {
+    setModalIsOpen(true)
+  }
+  const closeModalHandler = () => {
+    setModalIsOpen(false)
+  }
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <div>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
+      </div>
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
+    </div>
+  )
+}
+
+export default Todo
---
+++ src/pages/NewMeetups.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NewMeetupsPage = () => {
+  return <div>New Meetup Page</div>
+}
+
+export default NewMeetupsPage
